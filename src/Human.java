0bc4bafef3ce87e42764594940d982f827d8c357
Home <ildar86@live.ru>
2022-12-05 17:36:39 +0500

    дз-1. выполнено задание 1

---
+++ src/Human.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+
+public class Human {
+    int yearOfBirth;
+    String name;
+    String town;
+    int age;
+
+    public Human(int yearOfBirth, String name, String town) {
+        this.yearOfBirth = yearOfBirth;
+        this.name = name;
+        this.town = town;
+        age = LocalDate.now().getYear() - yearOfBirth;
+        /* System.out.println("Привет! Меня зовут " + name + ", я из города "
+        + town + ", мне " + age + ". Будем знакомы!");*/ // первый способ
+//       можно приветствие прописать в конструкторе, я выбрал вариант, который ближе к тому, что показали в видео-уроке
+            }
+
+    void sayHello() {
+        System.out.println("Привет! Меня зовут " + name + ", я из города "
+                + town + ", мне " + age + ". Будем знакомы!"); // второй способ, используя метод sayHello()
+    }
+
+   /* @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + ", я из города " + town + ", мне " + age + ". Будем знакомы!";
+    }*/     //третий способ, как можно вывести приветствие, используя метод toString()
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("        2 курс");
+        System.out.println();
+        System.out.println("    ООП. Урок 1 (введение, object)");
+        System.out.println();
+        Cat barsik = new Cat("Барсик", 18);
+        barsik.meow();
+        System.out.println();
+        Cat murzik = new Cat("Мурзик", 8);
+        murzik.meow();
+        Cat pushok = new Cat("Пушок", 7);
+        pushok.meow();
+        barsik.friends = null;
+        barsik.addFriend(murzik);
+        System.out.println("У кота " + barsik.name + "а " + barsik.getFriends().length + " друзей");
+        barsik.friends[0].meow();
+
+//        ДЗ
+//        Задание 1.
+
+        System.out.println("    ООП. Урок 1. ДЗ 1");
+        System.out.println("Задание 1");
+        System.out.println();
+        Human maksim = new Human(1988, "Максим", "Минск");
+        Human anna = new Human(1993, "Аня", "Москва");
+        Human katya = new Human(1992, "Катя", "Калининград");
+        Human artyom = new Human(1995, "Артём", "Москва");
+        maksim.sayHello();
+        anna.sayHello();
+        katya.sayHello();
+        artyom.sayHello();
+//        System.out.println(maksim); так можно вывести приветствие для каждого объекта,
+//        если использовать третий способ ( метод toString() )
+
     }
 }
