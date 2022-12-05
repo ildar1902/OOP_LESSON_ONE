3df32aa4a8fdd64df6214c18aa315c1159c75d3b
Home <ildar86@live.ru>
2022-12-05 19:35:45 +0500

    конструкторы дополнены проверками

--- src/Car.java
+++ src/Car.java
@@ -8,20 +8,44 @@ public class Car {
 
     public Car(String brand, String model, double engineVolume,
                String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        if (brand == null || model.isEmpty()) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null || model.isEmpty()) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null || color.isEmpty()) {
+            this.color = "white";
+        } else {
+            this.color = color;
+        }
+        if (year <= 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+        if (country == null || country.isEmpty()) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
     }
 
     @Override
     public String toString() {
-        return                 "brand = " + brand + ", model = " + model +
+        return "brand = " + brand + ", model = " + model +
                 ", engineVolume = " + engineVolume +
                 ", color = " + color +
                 ", year = " + year +
-                ", country = " + country ;
+                ", country = " + country;
     }
 }
--- src/Human.java
+++ src/Human.java
@@ -1,30 +1,42 @@
-import java.time.LocalDate;
-
 public class Human {
     int yearOfBirth;
     String name;
     String town;
-    int age;
     String jobTitle;
-
     public Human(int yearOfBirth, String name, String town, String jobTitle) {
-        this.yearOfBirth = yearOfBirth;
-        this.name = name;
-        this.town = town;
-        this.jobTitle = jobTitle;
-        age = LocalDate.now().getYear() - yearOfBirth; //немного доработал, чтобы каждый называл сразу свой возраст
+        if (yearOfBirth > 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
+        if (name == null || name.isEmpty()) {
+            this.name = "'Информация не указана'";
+        } else {
+            this.name = name;
+        }
+        if (town == null || town.isEmpty()) {
+           this.town = "'Информация не указана'";
+        } else {
+            this.town = town;
+        }
+        if (jobTitle == null || jobTitle.isEmpty()) {
+           this.jobTitle = "'Информация не указана'";
+        } else {
+            this.jobTitle = jobTitle;
+        }
         /* System.out.println("Привет! Меня зовут " + name + ", я из города "
-        + town + ", мне " + age + ". Будем знакомы!");*/ // первый способ
+        + town + ", я " + yearOfBirth + "-го года рождения. Будем знакомы!");*/ // первый способ
 //       можно приветствие прописать в конструкторе, я выбрал вариант, который ближе к тому, что показали в видео-уроке
     }
 
     void sayHello() {
         System.out.println("Привет! Меня зовут " + name + ", я из города "
-                + town + ", мне " + age + ". Я работаю на должности " + jobTitle + ". Будем знакомы!"); // второй способ, используя метод sayHello()
+                + town + ", я " + yearOfBirth + "-го года рождения. Я работаю на должности "
+                + jobTitle + ". Будем знакомы!"); // второй способ, используя метод sayHello()
     }
 
    /* @Override
     public String toString() {
-        return "Привет! Меня зовут " + name + ", я из города " + town + ", мне " + age + ". Будем знакомы!";
+        return "Привет! Меня зовут " + name + ", я из города " + town + ", я " + yearBirth + "-го года рождения. Будем знакомы!";
     }*/     //третий способ, как можно вывести приветствие, используя метод toString()
 }
